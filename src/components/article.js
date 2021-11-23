dfd0dbe6fc931dcaa91e401e40852243671fb2ef
Kurt <kin.kurt@gmail.com>
2021-11-23 16:04:15 +0200

    updated loading again

--- src/components/article.js
+++ src/components/article.js
@@ -4,7 +4,7 @@ import * as React from "react";
 import { Switch  } from '@headlessui/react'
 
 import "tailwindcss/tailwind.css";
-import {HeartIcon, ChatIcon, PaperAirplaneIcon,BookmarkIcon, EmojiHappyIcon} from '@heroicons/react/outline';
+import {HeartIcon, ChatIcon,BookmarkIcon, EmojiHappyIcon} from '@heroicons/react/outline';
 
 import { Fragment, useState } from "react";
 
@@ -100,7 +100,11 @@ const Article = ({data, searchItem}) => {
       const Body = () => {
         
         if (data == null) {
-            return <div className="animate-spin">Loading...</div>;
+            return <div className="flex">
+              <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+              <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+              <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+              </svg>Loading...</div>
           } 
           else if (data.errors) {
             return (
